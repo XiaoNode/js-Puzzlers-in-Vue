71ad73a00cdd47760f101d89b16b79d459d8e701
jone <905044086@qq.com>
2017-07-20 10:56:20 +0800

    修正页面

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="hello">
+    <img src="../assets/doyou.png">
     <h1>{{ title }}</h1>
-   <p v-html='msg'>
+   <p v-html='hmtlp'>
    </p>
-   <a class="button">start testing</a>
+   <!-- <a class="button">start testing</a> -->
+   <router-link to="item" class="button">start testing</router-link>
   </div>
 </template>
 
@@ -13,7 +15,7 @@ export default {
   data () {
     return {
       title: 'Do you really know JavaScript? Come JavaScript Puzzlers!',
-      msg: `This is a JavaScript quiz based on the behaviour as of <a href='http://www.ecma-international.org/ecma-262/5.1/#sec-15.4.4.19' target='_blank'>ECMA 262 (5.1)</a> in a browser environment.Behaviour may be different if you try it in a node or jsc REPL, for example this and global are different at the top level of a firefox console, node repl or jsc shell. Sorry about that.
+      hmtlp: `This is a JavaScript quiz based on the behaviour as of <a href='http://www.ecma-international.org/ecma-262/5.1/#sec-15.4.4.19' target='_blank'>ECMA 262 (5.1)</a> in a browser environment.Behaviour may be different if you try it in a node or jsc REPL, for example this and global are different at the top level of a firefox console, node repl or jsc shell. Sorry about that.
         <br>
         And more information can be accessed <a href='http://javascript-puzzlers.herokuapp.com/' target='_blank'>here</a>;
         <br>
@@ -30,9 +32,10 @@ h1{
   color: #EF8436;
 }
 p{
-  line-height: 25px;
+  line-height: 35px;
   text-align:left;
 }
+a{ text-decoration: none; }
 a.button{
   box-sizing: border-box;
   text-align: center;
