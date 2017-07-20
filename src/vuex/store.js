f29ace45371ac6d2daab0108f936031c9dffeadc
jone <905044086@qq.com>
2017-07-20 09:29:18 +0800

    添加vuex

---
+++ src/vuex/store.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex)
+
+const state={
+	count:1,
+	storemsg:'this msg is from store',
+	name:'薛陈磊',
+	sex:'man'
+}
+
+const mutations={
+	add(state,n){
+		console.log("mutations");
+		state.count+=n;
+	},
+	reduce(state){
+		state.count--
+	}
+}
+
+const getters={
+	count:function (state) {
+		console.log("getters");
+		return state.count+=10
+	}
+}
+
+  
+
+export default new Vuex.Store({
+	state,mutations,getters
+})
