d2d11783309d26c44fa460eaf8baf64d6990462d
jone <905044086@qq.com>
2017-07-20 17:59:31 +0800

    完善item页面

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -3,30 +3,32 @@
 	<div class="toptic">
 		<h1>{{rightnum}} / {{errornum}} of {{count}}</h1>
 	</div> 
-	<h2>What is the result of this expression? (or multiple ones)</h2>
-	<code>["1", "2", "3"].map(parseInt)</code>
-	<div class="buttondiv">
-		<button type="button" class="btn btn-default btn-lg">back blog</button>
-		<button type="button" class="btn btn-default btn-lg">back blog</button>
-		<button type="button" class="btn btn-default btn-lg">back blog</button>
-		<button type="button" class="btn btn-default btn-lg">back blog</button>		
+
+	<div v-for="item in itemss">
+		<h2>{{ item.title }}</h2>
+		<code>{{ item.question }}</code>
+		<div class="buttondiv">
+			<button type="button" class="btn btn-default btn-lg" @click="selected(index+1)" v-for="(i ,index) in item.select">{{ i }}</button>	
+		</div>
+
+		<p>{{ item.notic }}</p>
+
+		<img src="../assets/consider.png" class="img-responsive center-block" v-if="imgStatus=='consider'">
+		<img src="../assets/right.png" class="img-responsive center-block" v-if="imgStatus=='right'">
+		<img src="../assets/error.png" class="img-responsive center-block" v-if="imgStatus=='error'">
+
+		<div class="buttondiv">
+		<button type="button" class="btn btn-primary btn-lg fr" v-if="current<44 && showNext">Next test >></button> 
+		</div>	
 	</div>
 
-	<p>what you actually get is [1, NaN, NaN] because parseInt takes two parameters (val, radix) and map passes 3 (element, index, array)</p>
-
-	<img src="../assets/consider.png" class="img-responsive center-block" v-if="imgStatus=='consider'">
-	<img src="../assets/right.png" class="img-responsive center-block" v-if="imgStatus=='right'">
-	<img src="../assets/error.png" class="img-responsive center-block" v-if="imgStatus=='error'">
-
-	<div class="buttondiv">
-	<button type="button" class="btn btn-primary btn-lg fr" v-if="current<44 && showNext">Next test >></button> 
-	</div>
+	{{ indexof }}  ---
 </div>
 </template>
 
 <script>
 import store from '@/vuex/store'
-import { mapState, mapMutations } from 'vuex'
+import { mapState, mapMutations, mapGetters } from 'vuex'
 export default{
 	name: 'item',
 	data() {
@@ -36,12 +38,16 @@ export default{
 	},
 	store,
 	computed: {
-		...mapState(['count','rightnum','errornum','current','imgurl','imgStatus','showNext'])
+		...mapState(['count','rightnum','errornum','current','imgurl','imgStatus','showNext','items']),
+		...mapGetters(['itemss','indexof'])
 	},
 	methods: {
 		...mapMutations(['rightAdd','errorAdd']),
 		add(){
 			this.$store.commit('rightAdd')
+		},
+		selected(index){
+			console.log(index);
 		}
 	}
 }
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,10 +7,26 @@ const state={
 	count: 44,
 	rightnum: 0,
 	errornum: 0,
-	current: 1,
+	current: 0,
 	imgurl: '../assets/right.png',
 	imgStatus: 'consider',
-	showNext: false
+	showNext: false,
+	items: [
+		{
+			'title': 'What is the result of this expression? (or multiple ones)',
+			'question': '["1", "2", "3"].map(parseInt)',
+			'select': ['["1", "2", "3"]','[1, 2, 3]','[0, 1, 2]','other'],
+			 answer: '4',
+			'notic': 'what you actually get is [1, NaN, NaN] because parseInt takes two parameters (val, radix) and map passes 3 (element, index, array)'
+		},
+		{
+			'title': 'What is the result of this expression? (or multiple ones)',
+			'question': '[typeof null, null instanceof Object]',
+			'select': ['["object", false]','[null, false]','["object", true]','other'],
+			'answer': '1',
+			'notic': 'typeof will always return "object" for native non callable objects.'
+		}
+	]
 }
 
 const mutations={
@@ -23,9 +39,11 @@ const mutations={
 }
 
 const getters={
-	count:function (state) {
-		console.log("getters");
-		return state.count+=10
+	itemss:function (state) {  
+		return state.items.slice(state.current,state.current+1)
+	},
+	indexof:function (state) {
+		return state.items.slice(state.current,state.current+1).answer
 	}
 }
 
