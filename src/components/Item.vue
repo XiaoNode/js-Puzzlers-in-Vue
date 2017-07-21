936545f39134c7b26422d71499215ad1e9eab160
Jone <905044086@qq.com>
2017-07-21 23:20:29 +0800

    添加promise-auto ie支持

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -7,11 +7,11 @@
 	<div v-for="item in itemss">
 		<h2>{{ item.title }}</h2>
 		<code>{{ item.question }}</code>
-		<div class="buttondiv">
-			<button type="button" class="btn btn-default btn-lg" @click="selected(index+1)" v-for="(i ,index) in item.select">{{ i }}</button>	
+		<div class="buttondiv bt">
+			<button type="button" :class="claname" @click="selected(index+1,$event)" v-for="(i ,index) in item.select">{{ i }}</button>	
 		</div>
 
-		<p v-if="isselectright">{{ item.notic }}</p>
+		<p v-if="isselectright" class="bg-info"><b> Tip: </b> {{ item.notic }}</p>
 
 		<img src="../assets/consider.png" class="img-responsive center-block" v-if="imgStatus=='consider'">
 		<img src="../assets/right.png" class="img-responsive center-block" v-if="imgStatus=='right'">
@@ -20,7 +20,7 @@
 		<div class="buttondiv">
 		<button type="button" class="btn btn-primary btn-lg fr" v-if="current<44 && showNext" @click="nextPage">Next test >></button> 
 		</div>	 
-		isselected:{{isselected}},  isselectright:{{isselectright}}
+		<!--isselected:{{isselected}},  isselectright:{{isselectright}}-->
 	</div> 
 </div>
 </template>
@@ -32,20 +32,22 @@ export default{
 	name: 'item',
 	data() {
 		return {
-			msg: 'test begin!'
+			msg: 'test begin!',
+			claname: 'btn btn-default btn-lg'
 		}
 	},
 	store,
 	computed: {
-		...mapState(['count','rightnum','errornum','current','imgurl','imgStatus','showNext','items']),
-		...mapGetters(['itemss','answer','isselected','isselectright'])
+		...mapState(['count','rightnum','errornum','current','imgurl','imgStatus','showNext','items','isselected','isselectright']),
+		...mapGetters(['itemss','answer'])
 	},
 	methods: {
 		...mapMutations(['rightAdd','errorAdd','changeErrorImg','changeShowNext','nextpage','changeSelected','changeSelectright']),
 		add: function(){
 			this.$store.commit('rightAdd')
 		},
-		selected: function(index){
+		selected: function(index,e){
+			console.log(e.target.className);
 			// not selected any option
 			if(this.isselected==false){
 				if(index==this.answer){
@@ -54,22 +56,27 @@ export default{
 					this.rightAdd();
 					this.changeSelected();
 					this.changeSelectright();
+					e.target.className="btn btn-default btn-lg btn-success";
 				}else{
 					this.changeErrorImg({text:'error'});
 					this.errorAdd();
 					this.changeSelected();
+					e.target.className="btn btn-default btn-lg btn-danger";
 				}
 			}else{
 				// not select right
 				if(this.isselectright==false){
 					if(index==this.answer){
 						this.changeShowNext();
-						
 						this.changeSelected();
-						this.changeSelectright();						
+						this.changeSelectright();	
+						e.target.className="btn btn-default btn-lg btn-success";
+					}else{
+						e.target.className="btn btn-default btn-lg btn-danger";
 					}
 				}else{
 					this.changeSelected();
+					e.target.className="btn btn-default btn-lg btn-danger";
 				}
 			}  
 		},
@@ -77,6 +84,7 @@ export default{
 			this.nextpage();
 			this.changeErrorImg({text:'consider'});
 			this.changeShowNext();
+			
 		}
 	}
 }
@@ -88,6 +96,6 @@ h2{ margin-bottom: 30px }
 code{ font-size: 160% }
 .buttondiv{ margin: 30px 0 }
 .buttondiv button{ margin: 0 10px }
-p{ font-size: 25px }
+p{ font-size: 25px; padding: 10px; border-radius: 5px; }
 .fr{ float: right }
 </style>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -11,33 +11,29 @@ const state={
 	imgurl: '../assets/right.png',
 	imgStatus: 'consider',
 	showNext: false,
+	isselected: false,
+	isselectright: false,
 	items: [
 		{
 			'title': 'What is the result of this expression? (or multiple ones)',
 			'question': '["1", "2", "3"].map(parseInt)',
 			'select': ['["1", "2", "3"]','[1, 2, 3]','[0, 1, 2]','other'],
 			'answer': 4,
-			'notic': 'what you actually get is [1, NaN, NaN] because parseInt takes two parameters (val, radix) and map passes 3 (element, index, array)',
-			'selected': false,
-			'selectright': false
+			'notic': 'what you actually get is [1, NaN, NaN] because parseInt takes two parameters (val, radix) and map passes 3 (element, index, array)'
 		},
 		{
 			'title': 'What is the result of this expression? (or multiple ones)',
 			'question': '[typeof null, null instanceof Object]',
 			'select': ['["object", false]','[null, false]','["object", true]','other'],
 			'answer': 1,
-			'notic': 'typeof will always return "object" for native non callable objects.',
-			'selected': false,
-			'selectright': false
+			'notic': 'typeof will always return "object" for native non callable objects.'
 		},
 		{
 			'title': 'What is the result of this expression? (or multiple ones)',
 			'question': '[ [3,2,1].reduce(Math.pow), [].reduce(Math.pow) ]',
 			'select': ['an error','[9, 0]','[9, NaN]','[9, undefined]'],
 			'answer': 1,
-			'notic': 'Per spec: reduce on an empty array without an initial value throws TypeError',
-			'selected': false,
-			'selectright': false
+			'notic': 'Per spec: reduce on an empty array without an initial value throws TypeError'
 		}		
 	]
 }
@@ -49,16 +45,18 @@ const mutations={
 		state.imgStatus=text; 
 	},
 	changeShowNext: state => state.showNext=!state.showNext,
- 	changeSelected: state => state.items[state.current].selected=true,
- 	changeSelectright: state => state.items[state.current].selectright=true,
-	nextpage: state => state.current++
+ 	changeSelected: state => state.isselected=true,
+ 	changeSelectright: state => state.isselectright=true,
+	nextpage: state => {
+		state.current++;
+		state.isselected=false;
+		state.isselectright=false
+	}
 }
 
 const getters={
 	itemss: state => state.items.slice(state.current,state.current+1),
-	answer: state => state.items[state.current].answer,
-	isselected: state => state.items[state.current].selected,
-	isselectright: state => state.items[state.current].selectright
+	answer: state => state.items[state.current].answer
 }
 
 export default new Vuex.Store({
